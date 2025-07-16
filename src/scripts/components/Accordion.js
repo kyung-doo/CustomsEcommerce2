e3e9c577e7ee2c328699d7aa7c4f8808df30e21d
상록에스-웹사업\Sangrok_Dev <xowns4851491@gmail.com>
2025-07-16 17:07:35 +0900

    no message

--- src/scripts/components/Accordion.js
+++ src/scripts/components/Accordion.js
@@ -15,18 +15,21 @@ class Accordion {
 
     init () {
         this.ele.find('.accordion-header .accordion-btn').on('click', ( e ) => {            
-            const target = $(e.currentTarget).closest('.accordion-wrap')
+            const target = $(e.currentTarget).closest('.accordion-wrap');                           
             if(!target.hasClass('active')) {
                 if(this.props.beforeClose) {
+                    //다른 버튼 클릭하면 나머지 닫기
                     target.siblings('*[data-ui="accordion"]').find('.accordion-body').slideUp(100);
-                    target.siblings('*[data-ui="accordion"]').removeClass('active');
+                    target.siblings('*[data-ui="accordion"]').removeClass('active');                    
                 }
+                //열기
+                target.addClass('on'); 
                 target.find(".accordion-body").slideDown(100);
                 target.addClass('active');                
-                target.find('.accordion-btn').attr('title','닫기');
-                console.log(target.find('.accordion-btn'))
-            } else {
-                target.find(".accordion-body").slideUp(100);
+                target.find('.accordion-btn').attr('title','닫기');                
+            } else {                
+                //닫기
+                target.find(".accordion-body").slideUp(100);                
                 target.removeClass('active');
                 target.find('.accordion-btn').attr('title','열기');                
             }
