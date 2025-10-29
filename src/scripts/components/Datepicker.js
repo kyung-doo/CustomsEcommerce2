cb6eb2e61c6d312bf61573eac59743f0add91e13
상록에스-웹사업\Sangrok_Dev <xowns4851491@gmail.com>
2025-10-29 16:33:06 +0900

    no message

--- src/scripts/components/Datepicker.js
+++ src/scripts/components/Datepicker.js
@@ -55,7 +55,7 @@ class Datepicker {
             }
 
             if(!this.isValidDate(val)) {
-                // alert('올바른 날짜 형식을 입력하세요. (YYYY-MM-DD)');
+                //alert('올바른 날짜 형식을 입력하세요. (YYYY-MM-DD)');
                 //this.input.val('').focus();
                 ecp_alert('',ECP_MSG.err_ecp_ko_00031,this.input.val(''));
             } else {
