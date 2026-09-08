f296bf7e2a9fa277ce9a826eef152c9d5793a166
sidog0981 <sidog0981@naver.com>
2026-09-08 10:26:30 +0900

    이연주 대리 대전 출장에서의 수정 내용. (components.js) 1. Modal 클래스에서 스크롤 넓이 가져오는 함수 추가. (1296 line) 기존 스크롤 넓이 가져오는 정확도가 떨어져서 초반 페이지 렌더링 시 한 번 가져오고 로컬스토리지에 저장
    2. lockBodyScrollFn , unlockBodyScrollFn
    에서 전체 메뉴 펼쳤을때/없을때 화면 분기처리
    (body, header) 우측 패딩, overflow
    
    3. resize 될때 body에 overflow auto 주는 코드 제거

--- src/scripts/components/Modal.js
+++ src/scripts/components/Modal.js
@@ -1,17 +1,14 @@
-
 class Modal {
+    static DEFAULT_PROPS = {};
 
-    static DEFAULT_PROPS = {
-        
-    }
-
-    constructor( ele, props ) {
+    constructor(ele, props) {
         this.ele = ele;
         this.props = props;
         this.init();
+        this.scrollWidth = 0;
     }
 
-    init () {
+    init() {
         this.ele.find(".modal-wrap").append(
             `<button type="button" class="btn-close" id="popup-default-close" title="레이어팝업 닫기">
                 <i class="icon close"></i>
@@ -23,124 +20,132 @@ class Modal {
         this.ele.empty();
     }
 
-    lockBodyScroll () {
+    lockBodyScroll() {
+        /**
+         * , allmenu 열렸을때 || body에 스크롤 있을때 && body에 overflow auto 걸려있을때 header에 패딩줘야함.
+         */
+
+        if (!scW) scW = getScrollBarWidth();
+
         const $body = $("body");
-        const $scrollCompensationTargets = $('#masthead, #header, #header .header-in');
-        const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-        const bodyPaddingRight = parseFloat($body.css("padding-right")) || 0;
 
-        $body.data("modal-padding-right", $body.css("padding-right"));
-        $scrollCompensationTargets.each(function () {
-            const $target = $(this);
-            $target.data("modal-padding-right", $target.css("padding-right"));
-        });
-        $("body,html").css({'overflow': 'hidden'});
+        const headPdCon1 = $(".allmenu").hasClass("active");
 
-        if (scrollbarWidth > 0) {
-            $body.css("padding-right", bodyPaddingRight + scrollbarWidth);
-            $scrollCompensationTargets.each(function () {
-                const $target = $(this);
-                const targetPaddingRight = parseFloat($target.data("modal-padding-right")) || 0;
+        // 전체메뉴 펼쳤을때
+        if (headPdCon1) {
+            $body.css("padding-right", 0);
+            $("body,html").css({ overflow: "hidden" });
+            $("#header").css("padding-right", scW);
 
-                $target.css("padding-right", targetPaddingRight + scrollbarWidth);
-            });
+            return;
         }
+
+        $body.css("padding-right", scW);
+        $body.data("modal-padding-right", $body.css("padding-right"));
+
+        $("body,html").css({ overflow: "hidden" });
     }
 
-    unlockBodyScroll () {
+    unlockBodyScroll() {
+        if (!scW) scW = getScrollBarWidth();
+
         const $body = $("body");
-        const $scrollCompensationTargets = $('#masthead, #header, #header .header-in');
-        const bodyPaddingRight = $body.data("modal-padding-right");
 
-        $("body,html").css({'overflow': ''});
-        $body.css("padding-right", bodyPaddingRight || "");
-        $body.removeData("modal-padding-right");
-        $scrollCompensationTargets.each(function () {
-            const $target = $(this);
-            const targetPaddingRight = $target.data("modal-padding-right");
+        const headPdCon1 = $(".allmenu").hasClass("active");
+        const headPdCon2 = $("body").css("overflow") === "auto" && window.innerWidth - document.body.getBoundingClientRect().width > 0;
 
-            $target.css("padding-right", targetPaddingRight || "");
-            $target.removeData("modal-padding-right");
-        });
+        // 전체메뉴 펼쳐져있는상태
+        if (headPdCon1) {
+            $body.css("padding-right", 0);
+            $("#header").css("padding-right", scW);
+
+            return;
+        }
+
+        // 전체메뉴 펼치지 않고 body에 스크롤 있는 상태
+        if (!headPdCon1 && headPdCon2) {
+            $body.css("padding-right", 0);
+            $body.data("modal-padding-right", $body.css("padding-right"));
+            $("#header").css("padding-right", 0);
+        }
+
+        $("body,html").css({ overflow: "auto" });
     }
 
-    show () {
-        this.ele.empty().append(this.copyHtml);        
-        this.ele.removeClass('d-none');
+    show() {
+        this.ele.empty().append(this.copyHtml);
+        this.ele.removeClass("d-none");
 
         this.lockBodyScroll();
 
-        const modalWidth = this.ele.find('.modal-wrap').outerWidth();
+        const modalWidth = this.ele.find(".modal-wrap").outerWidth();
 
         if (modalWidth <= 766) {
             // 767 미만
-            this.ele.find('.board-top div.top-txt').hide();            
+            this.ele.find(".board-top div.top-txt").hide();
         } else {
             // 767 이상
-            this.ele.find('.board-top div.top-txt').show();
+            this.ele.find(".board-top div.top-txt").show();
         }
-        
+
         // gsap.set(this.ele.find(".modal-wrap"), {scale: 0.9, opacity: 0});
         // gsap.to(this.ele.find(".modal-wrap"), 0.4, {delay:0.1, scale: 1, opacity: 1, ease: Back.easeOut, onComplete: () => {
-            this.ele.trigger('modal-show');
-            
-            var seachInp = $('.wrap-search-area input');
-            var deleteBtn = $('.wrap-search-area button.sch-delete');          
-        
-            if (seachInp.val()) {            
-                deleteBtn.show();
-            } else {            
-                deleteBtn.hide();
-            }
+        this.ele.trigger("modal-show");
+
+        var seachInp = $(".wrap-search-area input");
+        var deleteBtn = $(".wrap-search-area button.sch-delete");
+
+        if (seachInp.val()) {
+            deleteBtn.show();
+        } else {
+            deleteBtn.hide();
+        }
         // }});
-        
 
         this.ele.find(".btn-close").on("click", () => {
             this.hide();
             //아이디 modalAlert가 있을경우 실행되는 이벤트
-            $('#modalAlert').remove();
+            $("#modalAlert").remove();
         });
         this.ele.find(".modal-close").on("click", () => {
             this.hide();
-        });        
+        });
 
-        if(this.ele.attr('id') === 'homepage' || this.ele.attr('id') === 'customs' || this.ele.attr('id') === 'in-search'){
-            $("body").css({'overflow': 'auto'});       
-            $('#homepage,#customs').hide();                                        
-            //this.ele.show();            
-            this.ele.removeClass('d-none');
-            this.ele.removeAttr('style');
-        }          
-        
+        if (this.ele.attr("id") === "homepage" || this.ele.attr("id") === "customs" || this.ele.attr("id") === "in-search") {
+            // $("body").css({ 'overflow': 'auto' });
+            $("#homepage,#customs").hide();
+            //this.ele.show();
+            this.ele.removeClass("d-none");
+            this.ele.removeAttr("style");
+        }
 
-        if(this.ele.attr('id') === 'in-search'){
-            $("body").css({'overflow': 'hidden'});       
+        if (this.ele.attr("id") === "in-search") {
+            $("body").css({ overflow: "hidden" });
             // $(window).resize(function(){
-            //     $("body").css({'overflow': 'hidden'});       
+            //     $("body").css({'overflow': 'hidden'});
             // })
         }
-        
-        //관련누리집, 세관 바로가기 클릭하면 닫기
-        $('.footer-quick-pop a').click(function(){
-            $("body").css({'overflow': 'auto'});       
-            $('#homepage,#customs').hide();                                        
-            //this.ele.show();            
-            this.ele.removeClass('d-none');
-            this.ele.removeAttr('style');
-        })
 
-        
-        this.ele.find(".btn-close").on("focusin", e => {
-            $(document).on('keydown.modal', (e) => {
-                if(e.key === 'Tab' && !e.shiftKey) {
+        //관련누리집, 세관 바로가기 클릭하면 닫기
+        $(".footer-quick-pop a").click(function () {
+            $("body").css({ overflow: "auto" });
+            $("#homepage,#customs").hide();
+            //this.ele.show();
+            this.ele.removeClass("d-none");
+            this.ele.removeAttr("style");
+        });
+
+        this.ele.find(".btn-close").on("focusin", (e) => {
+            $(document).on("keydown.modal", (e) => {
+                if (e.key === "Tab" && !e.shiftKey) {
                     this.ele.find(".modal-wrap").focus();
                     e.preventDefault();
                 }
             });
         });
-        this.ele.find(".modal-wrap").on("focusin", e => {
-            $(document).on('keydown.modal', (e) => {
-                if(e.key === 'Tab' && e.shiftKey) {
+        this.ele.find(".modal-wrap").on("focusin", (e) => {
+            $(document).on("keydown.modal", (e) => {
+                if (e.key === "Tab" && e.shiftKey) {
                     if ($(document.activeElement).is(this.ele.find(".modal-wrap"))) {
                         this.ele.find(".btn-close").focus();
                         e.preventDefault();
@@ -148,37 +153,39 @@ class Modal {
                 }
             });
         });
-        this.ele.find(".btn-close").on("focusout", e => {
-            $(document).off('keydown.modal');
+        this.ele.find(".btn-close").on("focusout", (e) => {
+            $(document).off("keydown.modal");
         });
-        this.ele.find(".modal-wrap").on("focusout", e => {
-            $(document).off('keydown.modal');
+        this.ele.find(".modal-wrap").on("focusout", (e) => {
+            $(document).off("keydown.modal");
         });
-        this.ele.find('*').removeAttr('tabindex')
-        this.ele.find(".modal-wrap").attr("tabindex", 0).focus();                
-    }
-    
-    hide () {
-        this.ele.trigger('modal-hide');
-        this.ele.addClass('d-none');
-        this.ele.find(".btn-close").off("click");
-        this.ele.find(".modal-close").off("click");
-        $("#wrap, .guide-wrap").removeAttr('inert');
-        this.unlockBodyScroll();
-        $(document).off('keydown.modal');
-        this.ele.empty();
-        $(`*[data-modal-target="#${this.ele.attr('id')}"]`).focus();        
+        this.ele.find("*").removeAttr("tabindex");
+        this.ele.find(".modal-wrap").attr("tabindex", 0).focus();
     }
 
+    hide() {
+        this.ele.trigger("modal-hide");
+        this.ele.addClass("d-none");
+        this.ele.find(".btn-close").off("click");
+        this.ele.find(".modal-close").off("click");
+        $("#wrap, .guide-wrap").removeAttr("inert");
+        this.unlockBodyScroll();
+        $(document).off("keydown.modal");
+        this.ele.empty();
+        $(`*[data-modal-target="#${this.ele.attr("id")}"]`).focus();
+
+        $("body").removeData("modal-padding-right");
+        $("body").css("padding-right", "0");
+    }
 }
 
 $.fn.modal = function (option, params) {
     return this.each(function () {
         var $this = $(this);
-        var data = $this.data('modal');
-        var options =  $.extend({}, Modal.DEFAULT_PROPS, typeof option == "object"  && option);
-        if(!data || typeof data == 'string') $this.data('modal', (data = new Modal($this, options)));
-        if(typeof option == 'string') data[option](params);
+        var data = $this.data("modal");
+        var options = $.extend({}, Modal.DEFAULT_PROPS, typeof option == "object" && option);
+        if (!data || typeof data == "string") $this.data("modal", (data = new Modal($this, options)));
+        if (typeof option == "string") data[option](params);
     });
 };
 $.fn.modal.Constructor = Modal;
